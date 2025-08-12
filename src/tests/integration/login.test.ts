0c071f8d440e8c1d60fd646b1dd84b25b3579b5e
zeonga1102 <zeonga1102@gmail.com>
2025-08-12 21:36:10 +0900

    TEST: 로그인 API 테스트 코드 작성
    POST /api/usres/login 테스트 코드 작성

---
+++ src/tests/integration/login.test.ts
@@ -0,0 +1,65 @@
+import request from 'supertest';
+
+import { DI, start } from '../../index';
+import app from '../../app';
+import { User } from '../../entities/User';
+import { ErrorMessages } from '../../constants/error-messages';
+
+const API = '/api/users/login';
+
+describe('로그인 API POST /api/users/login 통합 테스트', () => {
+  const existingData = {
+    username: 'login',
+    password: 'password',
+    name: 'user',
+    email: 'logintest@example.com'
+  };
+
+  beforeAll(async () => {
+    await start;
+    await DI.orm.getSchemaGenerator().refreshDatabase();
+  });
+
+  afterAll(async () => {
+    await DI.orm.close(true);
+    DI.server.close();
+  });
+
+  beforeEach(async () => {
+    DI.em = DI.orm.em.fork();
+    await DI.em.nativeDelete(User, {});
+
+    await request(app).post('/api/users/signup').send(existingData);
+  });
+
+  it('로그인 성공', async () => {
+    const result = await request(app).post(API).send(existingData);
+
+    expect(result.status).toBe(201);
+    expect(result.body).toHaveProperty('accessToken');
+  });
+
+  it('존재하지 않는 아이디로 로그인 시 실패', async () => {
+    const input = {
+      username: 'wrong',
+      password: existingData.password
+    };
+
+    const result = await request(app).post(API).send(input);
+
+    expect(result.status).toBe(401)
+    expect(result.body.message).toBe(ErrorMessages.LOGIN_FAILED);
+  });
+
+  it('비밀번호 틀릴 경우 실패', async () => {
+    const input = {
+      username: existingData.username,
+      password: 'wrong'
+    };
+
+    const result = await request(app).post(API).send(input);
+
+    expect(result.status).toBe(401)
+    expect(result.body.message).toBe(ErrorMessages.LOGIN_FAILED);
+  });
+});
