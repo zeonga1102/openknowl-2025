d386eb694f6a0339b8b7a4ba7403b3e917bad17d
zeonga1102 <zeonga1102@gmail.com>
2025-08-16 17:44:10 +0900

    STYLE: 인덴트 수정

--- src/dtos/CreateUserResponseDto.ts
+++ src/dtos/CreateUserResponseDto.ts
@@ -1,22 +1,22 @@
 import { Expose, Transform } from "class-transformer";
 
 export class CreateUserResponseDto {
-    @Expose()
-    id: number;
+  @Expose()
+  id: number;
 
-    @Expose()
-    username: string;
+  @Expose()
+  username: string;
 
-    @Expose()
-    name: string;
+  @Expose()
+  name: string;
 
-    @Expose()
-    email: string;
+  @Expose()
+  email: string;
 
-    @Expose()
-    @Transform(({ value }) => value ?? null)
+  @Expose()
+  @Transform(({ value }) => value ?? null)
     phone: string | null;
     
-    @Expose()
-    createdAt: Date;
+  @Expose()
+  createdAt: Date;
 }
