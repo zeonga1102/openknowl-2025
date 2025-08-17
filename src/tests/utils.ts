b2df5c8c75168903b21747d96d708006e8844adc
zeonga1102 <zeonga1102@gmail.com>
2025-08-17 13:14:25 +0900

    TEST: 만료된 토큰으로 요청하는 테스트 케이스 추가
    * 내 신청 내역 조회 시에 만료된 토큰으로 요청하는 케이스 추가
    * 만료된 토큰을 리턴하는 유틸 함수 생성

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,9 +1,11 @@
 import request from 'supertest';
 import { EntityManager } from '@mikro-orm/postgresql';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import app from '../app';
 import { User } from '../entities';
+import { ENV } from '../config';
 
 export async function getBearerToken(em: EntityManager, isAdmin: boolean = true) {
   const rand = Math.random().toString().substring(0, 8);
@@ -23,4 +25,31 @@ export async function getBearerToken(em: EntityManager, isAdmin: boolean = true)
   // 로그인 하여 토큰 획득
   const LoginResult = await request(app).post('/api/users/login').send({ username: user.username, password: 'password' });
   return `Bearer ${LoginResult.body.accessToken}`;
+}
+
+export async function getExpiredBearerToken(em: EntityManager, isAdmin: boolean = true) {
+  const rand = Math.random().toString().substring(0, 8);
+
+  const user = new User();
+  user.username = rand;
+  user.password = await bcrypt.hash('password', 10);;
+  user.name = rand;
+  user.email = `${rand}@example.com`;
+  user.isAdmin = isAdmin;
+
+  const userRepo = em.getRepository(User);
+  userRepo.create(user);
+  await em.flush();
+
+  const accessToken = jwt.sign(
+    {
+      id: user.id,
+      username: user.username,
+      isAdmin: user.isAdmin
+    },
+    ENV.JWT.SECRET_KEY,
+    { expiresIn: 0}
+  );
+
+  return `Bearer ${accessToken}`;
 }
