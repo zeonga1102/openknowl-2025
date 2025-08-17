832ac3cdfe5280555f4d8692eb0da886bc7c97dc
zeonga1102 <zeonga1102@gmail.com>
2025-08-17 10:35:34 +0900

    REFACTOR: M클래스 신청 시 assertApplication에서만 예외 던지도록 변경

--- src/services/mclassService.ts
+++ src/services/mclassService.ts
@@ -85,20 +85,11 @@ export async function applyToMClass(em: EntityManager, id: number, requestUser: 
       { id: id, isDelete: false },
       { lockMode: LockMode.PESSIMISTIC_WRITE }
     );
-    // 존재하지 않는 M클래스
-    if (!mclass) {
-      throw new NotFoundError();
-    }
-
-    // 본인이 만든 M클래스
-    if (mclass.createdUser.id == requestUser.id) {
-      throw new ForbiddenError(ErrorMessages.CAN_NOT_APPLY_TO_OWN_MCLASS);
-    }
-
+    
     await assertApplication(appRepo, mclass, requestUser.id);
 
     const application = new Application();
-    application.mclass = mclass;
+    application.mclass = mclass!;
     application.user = em.getReference(User, requestUser.id);
 
     appRepo.create(application);
@@ -112,15 +103,25 @@ export async function applyToMClass(em: EntityManager, id: number, requestUser: 
   }
 }
 
-async function assertApplication(appRepo: EntityRepository<Application>, mclass: MClass, userId: number) {
+async function assertApplication(appRepo: EntityRepository<Application>, mclass: MClass | null, userId: number) {
   const now = new Date();
+
+  // 존재하지 않는 M클래스
+  if (!mclass) {
+    throw new NotFoundError();
+  }
+
+  // 본인이 만든 M클래스
+  if (mclass.createdUser.id == userId) {
+    throw new ForbiddenError(ErrorMessages.CAN_NOT_APPLY_TO_OWN_MCLASS);
+  }
 
   // 이미 신청한 M클래스
   const application = await appRepo.findOne({ mclass: mclass, user: userId });
   if (application) {
     throw new ConflictError(ErrorMessages.ALREADY_APPLY);
   }
-  
+
   // 마감 시간 초과
   if (mclass.deadline <= now) {
     throw new ConflictError(ErrorMessages.DEADLINE_OVER);
